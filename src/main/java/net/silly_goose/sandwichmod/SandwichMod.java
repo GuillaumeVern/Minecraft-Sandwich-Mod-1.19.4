dee3a7ed54918f27a44471786a80d15878ff990b
Guillaume Vern <guillaume.vern@laposte.net>
2023-06-04 16:54:31 +0200

    Initial commit

---
+++ src/main/java/net/silly_goose/sandwichmod/SandwichMod.java
@@ -0,0 +1,16 @@
+package net.silly_goose.sandwichmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SandwichMod implements ModInitializer {
+	public static final String MOD_ID = "sandwichmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger("sandwichmod");
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/silly_goose/sandwichmod/SandwichModClient.java
@@ -0,0 +1,10 @@
+package net.silly_goose.sandwichmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SandwichModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
